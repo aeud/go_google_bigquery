aa3bf770400c1417fbc52d6cb9e56efbdef0d819
aeud <adrien.eudes@gmail.com>
2016-08-10 10:14:52 +0800

    First commit

---
+++ bigquery.go
@@ -0,0 +1,181 @@
+package go_google_bigquery
+
+import (
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	bigquery "google.golang.org/api/bigquery/v2"
+	"io/ioutil"
+	"log"
+	"time"
+)
+
+const (
+	SourceFormat     string = "NEWLINE_DELIMITED_JSON"
+	WriteDisposition string = "WRITE_TRUNCATE"
+)
+
+type BQField struct {
+	Ref *bigquery.TableFieldSchema
+}
+
+func NewBQField(n, t, d string) *BQField {
+	f := new(BQField)
+	f.Ref = new(bigquery.TableFieldSchema)
+	f.Ref.Mode = "NULLABLE"
+	f.Ref.Name = n
+	f.Ref.Type = t
+	f.Ref.Description = d
+	return f
+}
+
+func NewBQFieldWithNested(n, d string, nest *BQSchema) *BQField {
+	f := new(BQField)
+	f.Ref = new(bigquery.TableFieldSchema)
+	f.Ref.Mode = "NULLABLE"
+	f.Ref.Name = n
+	f.Ref.Type = "RECORD"
+	f.Ref.Description = d
+	f.Ref.Fields = nest.getRefFields()
+	return f
+}
+
+func NewBQFieldWithRepeated(n, d string, nest *BQSchema) *BQField {
+	f := new(BQField)
+	f.Ref = new(bigquery.TableFieldSchema)
+	f.Ref.Mode = "REPEATED"
+	f.Ref.Name = n
+	f.Ref.Type = "RECORD"
+	f.Ref.Description = d
+	f.Ref.Fields = nest.getRefFields()
+	return f
+}
+
+type BQSchema struct {
+	Fields []*BQField
+}
+
+func (s *BQSchema) getRefFields() []*bigquery.TableFieldSchema {
+	fs := s.Fields
+	rs := make([]*bigquery.TableFieldSchema, len(fs))
+	for i := 0; i < len(fs); i++ {
+		rs[i] = fs[i].Ref
+	}
+	return rs
+}
+
+func (s *BQSchema) getRefSchema() *bigquery.TableSchema {
+	t := new(bigquery.TableSchema)
+	t.Fields = s.getRefFields()
+	return t
+}
+
+func NewBQSchema(fs []*BQField) *BQSchema {
+	s := new(BQSchema)
+	s.Fields = fs
+	return s
+}
+
+func NewEmptyBQSchema() *BQSchema {
+	s := new(BQSchema)
+	s.Fields = make([]*BQField, 0)
+	return s
+}
+
+func (s *BQSchema) AddField(f *BQField) {
+	s.Fields = append(s.Fields, f)
+}
+
+type BQService struct {
+	Service  *bigquery.JobsService
+	ProjetID string
+	Schema   *BQSchema
+}
+
+func NewBQService(projectID, keyFile string) *BQService {
+	s := new(BQService)
+	data, err := ioutil.ReadFile(keyFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	conf, err := google.JWTConfigFromJSON(data, []string{bigquery.BigqueryScope}...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	client := conf.Client(oauth2.NoContext)
+	service, err := bigquery.New(client)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s.Service = bigquery.NewJobsService(service)
+	s.ProjetID = projectID
+	return s
+}
+
+func (s *BQService) NewJob(dataset, table, source string, schema *BQSchema) *BQJob {
+	job := new(BQJob)
+	job.Service = s
+	job.Schema = schema
+	job.Dataset = dataset
+	job.Table = table
+	job.Source = source
+	return job
+}
+
+type BQJob struct {
+	Service *BQService
+	Schema  *BQSchema
+	Dataset string
+	Table   string
+	Source  string
+}
+
+func (j *BQJob) GetTableRef() *bigquery.TableReference {
+	t := new(bigquery.TableReference)
+	t.DatasetId = j.Dataset
+	t.ProjectId = j.Service.ProjetID
+	t.TableId = j.Table
+	return t
+}
+
+func (j *BQJob) GetRefJob() *bigquery.Job {
+	load := new(bigquery.JobConfigurationLoad)
+	load.DestinationTable = j.GetTableRef()
+	load.Schema = j.Schema.getRefSchema()
+	load.SourceFormat = SourceFormat
+	load.SourceUris = []string{j.Source}
+	load.WriteDisposition = WriteDisposition
+	conf := new(bigquery.JobConfiguration)
+	conf.Load = load
+	job := new(bigquery.Job)
+	job.Configuration = conf
+	return job
+}
+
+func (j *BQJob) Do() {
+	service := j.Service.Service
+	projectID := j.Service.ProjetID
+
+	insertJob, err := service.Insert(projectID, j.GetRefJob()).Do()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	CheckJob(service.Get(projectID, insertJob.JobReference.JobId))
+}
+
+func CheckJob(c *bigquery.JobsGetCall) {
+	job, err := c.Do()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(job.Status.State)
+	if job.Status.State != "DONE" {
+		time.Sleep(time.Second)
+		CheckJob(c)
+	} else if job.Status.ErrorResult != nil {
+		log.Println(job.Status.ErrorResult)
+		for i := 0; i < len(job.Status.Errors); i++ {
+			log.Println(job.Status.Errors[i])
+		}
+	}
+}
---
+++ bigquery_test.go
@@ -0,0 +1,48 @@
+package go_google_bigquery
+
+import (
+	"testing"
+)
+
+func TestEmptySchema(t *testing.T) {
+	s := NewEmptyBQSchema()
+	s.AddField(NewBQField("foo", "STRING", "Test description"))
+}
+
+func TestSchema(t *testing.T) {
+	s := NewBQSchema([]*BQField{
+		NewBQField("foo", "STRING", "Test description"),
+	})
+	s.AddField(NewBQField("foo2", "STRING", "Test description 2"))
+}
+
+func TestNestedSchema(t *testing.T) {
+	s := NewBQSchema([]*BQField{
+		NewBQField("foo", "STRING", "Test description"),
+		NewBQFieldWithNested("foo", "Test description", NewBQSchema([]*BQField{
+			NewBQField("foo3", "STRING", "Test description 3"),
+		})),
+	})
+	s.AddField(NewBQField("foo2", "STRING", "Test description 2"))
+}
+
+func TestRepeatedSchema(t *testing.T) {
+	s := NewBQSchema([]*BQField{
+		NewBQField("foo", "STRING", "Test description"),
+		NewBQFieldWithRepeated("foo", "Test description", NewBQSchema([]*BQField{
+			NewBQField("foo3", "STRING", "Test description 3"),
+		})),
+	})
+	s.AddField(NewBQField("foo2", "STRING", "Test description 2"))
+}
+
+func TestExecuteJob(t *testing.T) {
+	schema := NewBQSchema([]*BQField{
+		schema.AddField(NewBQField("foo", "STRING", "Test description")),
+	})
+
+	service := NewBQService("luxola.com:luxola-analytics", "/Users/adrien/.ssh/google.json")
+	job := service.NewJob("go", "test_table", "gs://lx-ga/test.json.gz", schema)
+
+	job.Do()
+}
