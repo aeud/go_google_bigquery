f086ba0e929de77cb9b7b92de8e9f9a8cc2b118f
aeud <adrien.eudes@gmail.com>
2016-09-22 16:31:45 +0800

    Added test for V2

---
+++ v2/v2_test.go
@@ -0,0 +1,36 @@
+package v2
+
+import (
+	"testing"
+)
+
+func TestDo(t *testing.T) {
+	doc := `
+{
+    "fields": [
+        { "name": "date", "type": "TIMESTAMP", "mode": "NULLABLE", "description": "Date" },
+        { "name": "currency", "type": "STRING", "mode": "NULLABLE", "description": "Currency" },
+        {
+            "name": "unitsPer",
+            "type": "RECORD",
+            "description": "1 XXX in Currency",
+            "fields": [
+                { "name": "SGD", "type": "FLOAT", "mode": "NULLABLE" },
+                { "name": "EUR", "type": "FLOAT", "mode": "NULLABLE" }
+            ]
+        },
+        {
+            "name": "perUnit",
+            "type": "RECORD",
+            "description": "1 Currency in XXX",
+            "fields": [
+                { "name": "SGD", "type": "FLOAT", "mode": "NULLABLE" },
+                { "name": "EUR", "type": "FLOAT", "mode": "NULLABLE" }
+            ]
+        }
+    ]
+}
+    `
+	service := NewBQService("luxola.com:luxola-analytics", "/Users/adrien/.ssh/google.json")
+	service.NewJob("go", "xe", "gs://lx-ga/dwh/xe/*", doc).Do()
+}
